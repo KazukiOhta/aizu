60f70f572254fec9666bfdb3dc17a4cd38f4b9c0
太田　一毅 <kazukiota121@gmail.com>
2015-04-15 22:21:01 +0900

    Initial Commit

---
+++ Hello!World!/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Hello!World!
+//
+//  Created by 太田　一毅 on 2015/04/15.
+//  Copyright (c) 2015年 太田一毅. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
